16fd3e18be067f9d566bab4cda732040e0e11405
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-06-28 12:55:23 -0400

    stuck

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
-
 const fs = require('fs');
 const path = require('path');
 //request express file access from node_modules
@@ -9,19 +6,42 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 //assign express function to a variable to later chain on methods to Express.js server
 const app = express();
-
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true}));
-//getting access to public folder
-app.use(express.static('public'));
 //parse incoming JSON data
 app.use(express.json());
-//telling to use router
-app.use('api', apiRoutes);
-app.use('/', htmlRoutes);
 //creating a route to request data form
-const { notes } = require("./db/db.json")
-//make server listen to requests using the listen method
+const { notes } = require("./db/db.json");
+const { type } = require('os');
+//getting access to public folder
+app.use(express.static('public'));
+
+
+//function to ACCEPT POST data from req.body and add it to db.json
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+//function to validate note data
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
+
+
+
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`)
 });
